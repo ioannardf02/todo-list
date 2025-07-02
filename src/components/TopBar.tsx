6033a28dbe482cfa4049bc7218e7348d3180af71
Ioanna Rentifi <ion2002rdf@gmail.com>
2025-07-02 10:46:34 +0300

    fix(TopBar): change padding and text

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,13 +2,13 @@ import { NavLink } from "react-router";
 
 const TopBar = () => {
   return (
-    <div className="flex flex-row justify-between p-6 items-center">
+    <div className="flex flex-row justify-between p-2 items-center">
       <div className="flex flex-row gap-2 items-center">
         <img
           src="https://cdn-icons-png.flaticon.com/512/8019/8019152.png"
           className="h-10"
         />
-        <h1 className="text-2xl font-medium text-fuchsia-900">EASY LIST</h1>
+        <h1 className="text-2xl font-medium text-fuchsia-900">EASY PLAN</h1>
       </div>
       <NavLink to={"/"} className="text-2xl font-bold  text-fuchsia-800">
         Home
