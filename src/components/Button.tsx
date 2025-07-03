24270c84b07d3faa3118d9998fbac28153bb9074
Ioanna Rentifi <ion2002rdf@gmail.com>
2025-07-03 17:28:34 +0300

    feat(addTask):try to add task

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,18 @@
+import type { MouseEventHandler } from "react";
+
 const Button = ({
   text,
   btnStyle,
   imgSrc,
+  addTask,
 }: {
   text?: string;
   btnStyle: string;
   imgSrc?: string;
+  addTask?: MouseEventHandler;
 }) => {
   return (
-    <button className={btnStyle}>
+    <button className={btnStyle} onClick={addTask}>
       {text}
       {imgSrc && <img src={imgSrc} />}
     </button>
--- src/components/listPage/List.tsx
+++ src/components/listPage/List.tsx
@@ -1,11 +1,11 @@
 import AddTask from "./AddTask";
-import InputTask from "./InputTask";
+//import InputTask from "./InputTask";
 
 const List = () => {
   return (
     <div className="flex flex-col rounded-3xl bg-violet-50 shadow-2xl items-center p-10 gap-7 lg:w-1/2 md:w-1/2 w-full text-center">
       <AddTask />
-      <InputTask />
+      {/* <InputTask /> */}
     </div>
   );
 };
